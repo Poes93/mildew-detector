f5550a6b21c1acb057e867de18d716e5fbca9b89
Daniel Ahlberg <dahlberg340@gmail.com>
2024-04-05 11:58:43 +0000

    update streamlit browse files

--- app_pages/page_powdery_mildew_detector.py
+++ app_pages/page_powdery_mildew_detector.py
@@ -35,8 +35,7 @@ def page_powdery_mildew_detector_body():
     if images_buffer is not None:
         df_report = pd.DataFrame([])
         for image in images_buffer:
-
-            img_pil = (Image.open(image))
+            img_pil = Image.open(image)
             st.info(f"Cherry Leaf Sample Image: **{image.name}**")
             img_array = np.array(img_pil)
             st.image(img_pil, caption=f"Image Size: {img_array.shape[1]}px width x {img_array.shape[0]}px height")
@@ -46,8 +45,7 @@ def page_powdery_mildew_detector_body():
             pred_prob, pred_class = load_model_and_predict(resized_img, version=version)
             plot_predictions_probabilities(pred_prob, pred_class)
 
-            df_report = df_report.append({"Name":image.name, 'Result': pred_class },
-                                        ignore_index=True)
+            df_report = df_report.append({"Name": image.name, 'Result': pred_class}, ignore_index=True)
         
         if not df_report.empty:
             st.success("Analysis Report")
